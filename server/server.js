8b58f8ebc0a46694cadc8829a8d4a529753da1af
ashamsu12 <shamsudeenabubakar59@gmail.com>
2018-06-22 13:23:41 +0100

    67

--- server/server.js
+++ server/server.js
@@ -9,7 +9,82 @@
   server.listen(port, () => {
     console.log('Server listening at port %d', port);
   });
-  
+  class Queue {
+    constructor() {
+  this.head = undefined;
+  this.tail = undefined;
+  this.length = 0;
+}
+
+ push(data) {
+  this.add({ data: data, prev: this.tail, next: undefined });
+  return this;
+}
+
+pop() {
+  return this.tail && this.remove(this.tail).data;
+}
+
+shift() {
+  return this.head && this.remove(this.head).data;
+}
+
+unshift (data) {
+  this.add({ data: data, prev: undefined, next: this.head });
+  return this;
+}
+
+add(item) {
+  if(!item) return undefined;
+
+  if(item.prev) item.prev.next = item;
+  if(item.next) item.next.prev = item;
+
+  if(item.prev == undefined) this.head = item;  // add before head
+  if(item.next == undefined) this.tail = item;  // add to end
+
+  this.length++;
+
+  return item;
+}
+
+remove(item) {
+  if(!item) return undefined;
+
+  if(this.head == item) this.head = item.next;  // removing head
+  if(this.tail == item) this.tail = item.prev;  // removing tail
+
+  if(item.prev) item.prev.next = item.next;
+  if(item.next) item.next.prev = item.prev;
+
+  this.length--;
+
+  return item;
+}
+
+concat(queue) {
+  if(this.head == undefined) {
+    this.head = queue.head;
+    this.tail = queue.tail;
+    this.length = queue.length;
+    return this;
+  }
+  queue.head.prev = this.tail;
+  this.tail.next = queue.head;
+  this.tail = queue.tail;
+  this.length += queue.length;
+  return this;
+}
+
+// Use this to implement sort, insert, filter, take, drop, fold, map
+forEach(func, condition) {
+  for(var current = this.head; current; current = current.next) {
+    if(condition && !condition(current.data)) break;
+    func(current.data);
+  }
+}
+
+}
      
   // Chatroom
   const departments = ["finance","admissions","registration","student affairs", "documentation"];
@@ -72,82 +147,7 @@ let currentFin = 0, currentAd = 0, currentReq = 0, currentSA = 0, currentDoc = 0
   });
 
 
-  class Queue {
-        constructor() {
-      this.head = undefined;
-      this.tail = undefined;
-      this.length = 0;
-    }
-    
-     push(data) {
-      this.add({ data: data, prev: this.tail, next: undefined });
-      return this;
-    }
-    
-    pop() {
-      return this.tail && this.remove(this.tail).data;
-    }
-    
-    shift() {
-      return this.head && this.remove(this.head).data;
-    }
-    
-    unshift (data) {
-      this.add({ data: data, prev: undefined, next: this.head });
-      return this;
-    }
-    
-    add(item) {
-      if(!item) return undefined;
-    
-      if(item.prev) item.prev.next = item;
-      if(item.next) item.next.prev = item;
-    
-      if(item.prev == undefined) this.head = item;  // add before head
-      if(item.next == undefined) this.tail = item;  // add to end
-    
-      this.length++;
-    
-      return item;
-    }
-    
-    remove(item) {
-      if(!item) return undefined;
-    
-      if(this.head == item) this.head = item.next;  // removing head
-      if(this.tail == item) this.tail = item.prev;  // removing tail
-    
-      if(item.prev) item.prev.next = item.next;
-      if(item.next) item.next.prev = item.prev;
-    
-      this.length--;
-    
-      return item;
-    }
-    
-    concat(queue) {
-      if(this.head == undefined) {
-        this.head = queue.head;
-        this.tail = queue.tail;
-        this.length = queue.length;
-        return this;
-      }
-      queue.head.prev = this.tail;
-      this.tail.next = queue.head;
-      this.tail = queue.tail;
-      this.length += queue.length;
-      return this;
-    }
-    
-    // Use this to implement sort, insert, filter, take, drop, fold, map
-    forEach(func, condition) {
-      for(var current = this.head; current; current = current.next) {
-        if(condition && !condition(current.data)) break;
-        func(current.data);
-      }
-    }
-    
-  }
+ 
 
 
 
