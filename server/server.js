794b6ccfc9fd0cf9eed57dfaa56b0c8e917c69b4
ashamsu12 <shamsudeenabubakar59@gmail.com>
2018-06-22 13:15:27 +0100

    queue class added

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@
   var path = require('path');
   var server = require('http').createServer(app);
   const io = require('socket.io')(server);
-  var Queue = require('queue.js');
   var port = process.env.PORT || 3000;
   
   server.listen(port, () => {
@@ -34,7 +33,7 @@ let currentFin = 0, currentAd = 0, currentReq = 0, currentSA = 0, currentDoc = 0
     socket.on('finance', (data) => {
       // we tell the client to execute 'new message'
       var token  = "F" + (1000 + currentFin);
-      finance.enqueue('{"token" : token, "attended" : false}')
+      finance.enqueue({"token" : token, "attended" : false})
       io.of('/').sockets[socket.id].emit('finance', {
         message: token
       });
@@ -73,6 +72,82 @@ let currentFin = 0, currentAd = 0, currentReq = 0, currentSA = 0, currentDoc = 0
   });
 
 
+  class Queue {
+        constructor() {
+      this.head = undefined;
+      this.tail = undefined;
+      this.length = 0;
+    }
+    
+     push(data) {
+      this.add({ data: data, prev: this.tail, next: undefined });
+      return this;
+    }
+    
+    pop() {
+      return this.tail && this.remove(this.tail).data;
+    }
+    
+    shift() {
+      return this.head && this.remove(this.head).data;
+    }
+    
+    unshift (data) {
+      this.add({ data: data, prev: undefined, next: this.head });
+      return this;
+    }
+    
+    add(item) {
+      if(!item) return undefined;
+    
+      if(item.prev) item.prev.next = item;
+      if(item.next) item.next.prev = item;
+    
+      if(item.prev == undefined) this.head = item;  // add before head
+      if(item.next == undefined) this.tail = item;  // add to end
+    
+      this.length++;
+    
+      return item;
+    }
+    
+    remove = function(item) {
+      if(!item) return undefined;
+    
+      if(this.head == item) this.head = item.next;  // removing head
+      if(this.tail == item) this.tail = item.prev;  // removing tail
+    
+      if(item.prev) item.prev.next = item.next;
+      if(item.next) item.next.prev = item.prev;
+    
+      this.length--;
+    
+      return item;
+    }
+    
+    concat(queue) {
+      if(this.head == undefined) {
+        this.head = queue.head;
+        this.tail = queue.tail;
+        this.length = queue.length;
+        return this;
+      }
+      queue.head.prev = this.tail;
+      this.tail.next = queue.head;
+      this.tail = queue.tail;
+      this.length += queue.length;
+      return this;
+    }
+    
+    // Use this to implement sort, insert, filter, take, drop, fold, map
+    forEach(func, condition) {
+      for(var current = this.head; current; current = current.next) {
+        if(condition && !condition(current.data)) break;
+        func(current.data);
+      }
+    }
+    
+  }
 
 
 
